341f9a5e9ab42b0d82c4f783c160c220f3ac4e06
m.wrona <michal.robert.wronski@gmail.com>
2014-04-28 22:46:44 +0200

    Attaching Git prompt to current session during installation added.

--- install.sh
+++ install.sh
@@ -9,11 +9,17 @@ cd ~/
 echo "Installing GIT prompt in ~/$GIT_PROMPT_DIR"
 rm -rf $GIT_PROMPT_DIR 2>/dev/null 
 mkdir $GIT_PROMPT_DIR
-cp $SOURCE_DIR/git-prompt.sh $GIT_PROMPT_DIR
 cp $SOURCE_DIR/git-utils.sh $GIT_PROMPT_DIR
+cp $SOURCE_DIR/git-prompt.sh $GIT_PROMPT_DIR
 
 #attach scripts to .bashrc
 echo "Attaching sources of GIT prompt to ~/.bashrc"
 echo "source ~/$GIT_PROMPT_DIR/git-utils.sh" >> ~/.bashrc
 echo "source ~/$GIT_PROMPT_DIR/git-prompt.sh" >> ~/.bashrc
+
+#source GIT prompt to current session
+echo "Attaching GIT prompt to current shell session"
+cd $SOURCE_DIR
+source git-utils.sh
+source git-prompt.sh
 
